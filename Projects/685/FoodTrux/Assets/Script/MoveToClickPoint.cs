454097d5a60bd8a34dceb288e917bfeb6b23aab8
mcs1988 <mcs1988@gmail.com>
2016-10-27 04:16:08 -0400

    click to move functionality
    Added click to move functionality, obstacles and a nav mesh for
    obstacles.

---
+++ Projects/685/FoodTrux/Assets/Script/MoveToClickPoint.cs
@@ -0,0 +1,30 @@
+﻿// MoveToClickPoint.cs
+using UnityEngine;
+
+public class MoveToClickPoint : MonoBehaviour {
+	public Transform destination;
+
+	private NavMeshAgent agent;
+
+	void Start () 
+	{
+		agent = gameObject.GetComponent<NavMeshAgent>();
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			int layerMask = 1 << 8;
+			layerMask = ~layerMask;
+
+			Ray screenRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			RaycastHit hit;
+
+			if (Physics.Raycast(screenRay, out hit, Mathf.Infinity, layerMask)) {
+				agent.destination = hit.point;
+			}
+		}
+	}
+}
