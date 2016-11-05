8f012ca9bdb53bdd71c5d21f1ba71907ef4b4174
robertschifferli <robertschifferli@gmail.com>
2016-11-05 04:41:13 -0400

    Smoothed movement and added basic base building

--- Projects/685/FoodTrux/Assets/Script/MoveToClickPoint.cs
+++ Projects/685/FoodTrux/Assets/Script/MoveToClickPoint.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class MoveToClickPoint : MonoBehaviour {
 	public Transform destination;
 	public bool canMove = true;
+	public bool isMoving = false;
 
 	private NavMeshAgent agent;
 
@@ -12,8 +13,8 @@ public class MoveToClickPoint : MonoBehaviour {
 	private float destinationDistance;     //distance to destination
 
 	public float moveSpeed = 25.0f;    // controls character move speed
-	public float rotateSpeed = 90f;   // speed at which character will rotate
-	public float stopDistance = 10f; // stop moving when closer to stopDistance
+	public float rotateSpeed = 250f;   // speed at which character will rotate
+	public float stopDistance = 3f; // stop moving when closer to stopDistance
 
 	private Quaternion targetRotation;
 
@@ -22,15 +23,7 @@ public class MoveToClickPoint : MonoBehaviour {
 		myTransform = transform;   // cache transform to improve performance
 		destinationPosition = myTransform.position; // initialize destinationPosition
 		agent = gameObject.GetComponent<NavMeshAgent>();
-		GameObject[] trucks = GameObject.FindGameObjectsWithTag ("Truck");
-		foreach (GameObject truck in trucks) {
-			MeshRenderer[] renderers = truck.GetComponentsInChildren<MeshRenderer>();
-			foreach (MeshRenderer r in renderers) {
-				foreach (Material m in r.materials) {
-					m.color = Color.green;
-				}
-			}
-		}
+		changeTruckColor (Color.green);
 	}
 
 //	void Update()
@@ -56,45 +49,57 @@ public class MoveToClickPoint : MonoBehaviour {
 //	}
 
 	void Update () {
-		// will move player to clicked point
-		if (Input.GetMouseButtonDown(0)&& GUIUtility.hotControl == 0) {
-			// if some point clicked...
-			Plane playerPlane = new Plane(Vector3.up, myTransform.position);
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			float hitdist = 0.0f;
-			if (playerPlane.Raycast(ray, out hitdist)) {
-				// set destination to the point clicked
-				destinationPosition = ray.GetPoint(hitdist);
-			}//end nested if
-			Debug.Log ("CLICKED!");
-		}//end if
-		// calculate the current target direction
-		Vector3 destDir = destinationPosition - myTransform.position;
-		destDir.y = 0; // make it strictly horizontal to avoid object tilting
-		destinationDistance = destDir.magnitude; // get the horizontal distance
-		// object doesn't anything if below stopDistance:
-		if (destinationDistance >= stopDistance){ // if farther than stopDistance...
-			targetRotation = Quaternion.LookRotation(destDir); // update target rotation...
-			// turn gradually to target direction each frame:
-			myTransform.rotation = Quaternion.RotateTowards(myTransform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
-			// move in its local forward direction (Translate default):
-			myTransform.Translate(Vector3.forward * 10f * Time.deltaTime);  // move in forward direction 
-		}//end if
-
-		if (Input.GetKeyDown (KeyCode.D)) {
-			DeployTruck ();
+		if (canMove) {
+			// will move player to clicked point
+			if (Input.GetMouseButtonDown (0) && GUIUtility.hotControl == 0) {
+				// if some point clicked...
+				Plane playerPlane = new Plane (Vector3.up, myTransform.position);
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+				float hitdist = 0.0f;
+				if (playerPlane.Raycast (ray, out hitdist)) {
+					// set destination to the point clicked
+					destinationPosition = ray.GetPoint (hitdist);
+				}//end nested if
+				Debug.Log ("CLICKED!");
+			}//end if
+			// calculate the current target direction
+			Vector3 destDir = destinationPosition - myTransform.position;
+			Debug.Log ("dest dir = " + destDir + "    dest pos = " + destinationPosition + "     trans pos = " + myTransform.position);
+			destDir.y = 0; // make it strictly horizontal to avoid object tilting
+			destinationDistance = destDir.magnitude; // get the horizontal distance
+			// object doesn't anything if below stopDistance:
+			if (destinationDistance >= stopDistance) { // if farther than stopDistance...
+				isMoving = true;
+				changeTruckColor(Color.yellow);
+				targetRotation = Quaternion.LookRotation (destDir); // update target rotation...
+				// turn gradually to target direction each frame:
+				myTransform.rotation = Quaternion.RotateTowards (myTransform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+				// move in its local forward direction (Translate default):
+				myTransform.Translate (Vector3.forward * 25f * Time.deltaTime);  // move in forward direction 
+			}//end if
+			else{
+				isMoving = false;
+				changeTruckColor(Color.green);
+			}
+		}
+		
+		if (Input.GetKeyDown (KeyCode.D) && !isMoving) {
+			canMove = !canMove;
+			changeTruckColor (Color.red);
+		} else if (Input.GetKeyDown (KeyCode.D) && !canMove) {
+			canMove = !canMove;
+			changeTruckColor (Color.green);
 		}
 	}//end update
 
-	void DeployTruck()
+	void changeTruckColor(Color color)
 	{
-		canMove = false;
 		GameObject[] trucks = GameObject.FindGameObjectsWithTag ("Truck");
 		foreach (GameObject truck in trucks) {
 			MeshRenderer[] renderers = truck.GetComponentsInChildren<MeshRenderer>();
 			foreach (MeshRenderer r in renderers) {
 				foreach (Material m in r.materials) {
-					m.color = Color.red;
+					m.color = color;
 				}
 			}
 		}
