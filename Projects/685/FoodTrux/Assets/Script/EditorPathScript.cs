d12a0ca0ec64cebae964d55909496be13da88927
mcs1988 <mcs1988@gmail.com>
2016-11-18 03:25:52 -0500

    added path editor script for truck moving into garage animation

---
+++ Projects/685/FoodTrux/Assets/Script/EditorPathScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EditorPathScript : MonoBehaviour {
+
+	public Color rayColor = Color.white;
+	public List<Transform> path_objs = new List<Transform> ();
+	Transform[] pointArray;
+
+	void OnDrawGizmos () {
+		Gizmos.color = rayColor;
+		pointArray = GetComponentsInChildren<Transform> ();
+		path_objs.Clear ();
+
+		foreach (Transform path_obj in pointArray) {
+			if (path_obj != this.transform) {
+				path_objs.Add (path_obj);
+			}
+		}
+
+		for (int i = 0; i < path_objs.Count; i++) {
+			Vector3 position = path_objs [i].position;
+			if (i > 0) {
+				Vector3 previous = path_objs [i - 1].position;
+				Gizmos.DrawLine (previous, position);
+				Gizmos.DrawWireSphere (position, 3.0f);
+			}
+		}
+	}
+}
---
+++ Projects/685/FoodTrux/Assets/Script/MoveOnPathScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveOnPathScript : MonoBehaviour {
+
+	public EditorPathScript PathToFollow;
+
+	public int CurrentWayPointID = 0;
+	public float speed;
+	private float reachDistance = 1.0f;
+	public float RotationSpeed = 0.5f;
+	public string pathName;
+
+	Vector3 last_position;
+	Vector3 current_position;
+
+
+	// Use this for initialization
+	void Start () {
+		//PathToFollow = GameObject.Find (pathName).GetComponent<EditorPathScript> ();
+		last_position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float distance = Vector3.Distance (PathToFollow.path_objs[CurrentWayPointID].position, transform.position);
+		transform.position = Vector3.MoveTowards (transform.position, PathToFollow.path_objs [CurrentWayPointID].position, Time.deltaTime * speed);
+
+		if (distance <= reachDistance) {
+			CurrentWayPointID++;
+		}
+
+		if (CurrentWayPointID >= PathToFollow.path_objs.Count) {
+			CurrentWayPointID = 0;
+		}
+	}
+}
