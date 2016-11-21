b46cfff10f15e4ff641ee08e8cb8ba23a6c0bae8
mcs1988 <mcs1988@gmail.com>
2016-11-21 03:59:15 -0500

    homeBase is now Selectable as well
    Selection Logic still needs to be implemented

---
+++ Projects/685/FoodTrux/Assets/Script/baseClicked.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class baseClicked : MonoBehaviour {
+
+
+	public Canvas canvas;
+	bool isSelected = false;
+
+	private UnityEngine.NavMeshAgent agent;
+
+
+	// Use this for initialization
+	void Start () {
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (isSelected)
+			canvas.enabled = true;
+		else
+			canvas.enabled = false;
+	}
+
+	void OnMouseUpAsButton() {
+		//canvas.enabled = !canvas.enabled;
+		isSelected = !isSelected;
+		baseIsSelected ();
+		Debug.Log ("Base Has Been Clicked.");
+	}
+
+	void baseIsSelected()
+	{
+		GameObject selected = GameObject.Find("basePlane");
+
+		//GameObject selected = GameObject.Find("Truck"+idNum);
+
+
+		MeshRenderer rendered = selected.GetComponent<MeshRenderer>();
+
+			foreach (Material m in rendered.materials)
+			{
+				Color color = m.GetColor ("_OutlineColor");
+				color.a = isSelected ? 255.0f : 0.0f;
+				m.SetColor ("_OutlineColor", color);
+
+			}
+		}
+}
+
