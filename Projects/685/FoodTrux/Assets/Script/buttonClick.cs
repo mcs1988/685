01adec7722d4f388d3e25f20b38f0cbd1571bd19
mcs1988 <mcs1988@gmail.com>
2016-11-23 03:11:04 -0500

    VeryBasic NewTruck Button Functioning
    Button for "New Truck" creates duplicate Truck that somewhat functions
    properly.

---
+++ Projects/685/FoodTrux/Assets/Script/buttonClick.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class buttonClick : MonoBehaviour {
+	public Button yourButton;
+	//public Vector3 spawnPoint = Vector3.zero;
+
+	Vector3 position = new Vector3 (300.5f, 0f, 402.4f);
+
+
+
+	void Start () {
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+		//position = transform.position.Set(514.5f,0f,402.4f);
+	}
+
+	void TaskOnClick(){
+		Debug.Log ("You have clicked the button!");
+
+
+		//GameObject truckToCopy= GameObject.Find("Truck"+idNum);
+		GameObject truckToCopy= GameObject.Find("Truck1");
+
+		GameObject newTruck = Instantiate(truckToCopy, position, transform.rotation) as GameObject;
+
+		//Object.Instantiate (truckToCopy, position, Quaternion.identity);
+	}
+}
